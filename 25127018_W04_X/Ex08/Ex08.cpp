75a841bff86e9de9f7e1d6e715647c0f561e9d8b
Kevincrazy678 <gnguyenphuc9@gmail.com>
2026-03-09 07:07:22 +0700

    init project

--- 25127018_W04_X/Ex08/Ex08.cpp
+++ 25127018_W04_X/Ex08/Ex08.cpp
@@ -2,10 +2,14 @@
 class Solution {
 public:
     vector<vector<int>> sortMatrix(vector<vector<int>>& grid) {
+        //time complexity: O(n^2 * logn) + O(n^2) = O(n^2 * logn)
+        //space complexity: O(n)
         int n = grid.size();
+        //we are sorting the diagonals of the matrix in non-increasing order
         for(int i=0; i<n; i++){
             vector<int>tmp;
             int row = i, col = 0;
+            //we are storing the elements of the diagonal in a temporary vector and then sorting it in non-increasing order
             while(row < n && col < n){
                 tmp.push_back(grid[row][col]);
                 row++;
@@ -13,15 +17,18 @@ public:
             }
             sort(tmp.begin(), tmp.end(), greater<int>());
             row = i, col = 0;
+            //we are storing the sorted elements of the diagonal back in the matrix
             for(int x : tmp){
                 grid[row][col] = x;
                 row++;
                 col++;
             }
         }
+        //we are sorting the diagonals of the matrix in non-decreasing order
         for(int j=1; j<n; j++){
             vector<int>tmp;
             int row = 0, col = j;
+            //we are storing the elements of the diagonal in a temporary vector and then sorting it in non-decreasing order
             while(row < n && col < n){
                 tmp.push_back(grid[row][col]);
                 row++;
@@ -29,6 +36,7 @@ public:
             }
             sort(tmp.begin(), tmp.end());
             row = 0, col = j;
+            //we are storing the sorted elements of the diagonal back in the matrix
             for(int x : tmp){
                 grid[row][col] = x;
                 row++;
---
+++ 25127018_W04_X/README.md
@@ -0,0 +1,3 @@
+I didn't use any characteristics of AI tools to finish these excercises. But I have been using google, coursera and dictionary webs like sohatratu or CambridgeDictionary to find meanings of words I don't know.
+I've brainstormed for quite a long time to do these things.
+
